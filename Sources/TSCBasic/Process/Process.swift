f09fe14e167de8379f1712ebffb9804fd61b2bf5
Kila2 <lijunliang9819@qq.com>
2025-01-18 17:18:24 +0800

    fix missing env

--- Sources/TSCBasic/Process/Process.swift
+++ Sources/TSCBasic/Process/Process.swift
@@ -614,7 +614,7 @@ public final class Process {
             resolvedArgs[0] = executablePath.pathString
         }
         let argv = CStringArray(resolvedArgs)
-        let env = CStringArray(ProcessEnv.block.map({ "\($0.0)=\($0.1)" }))
+        let env = CStringArray(environmentBlock.map({ "\($0.0)=\($0.1)" }))
         let rv = posix_spawnp(&processID, argv.cArray[0]!, &fileActions, &attributes, argv.cArray, env.cArray)
 
         guard rv == 0 else {
